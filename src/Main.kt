7a2d5ebe22afab5286a38d1ad96a697f09fd1542
Josh LaSota <jlasota126@students.mchenry.edu>
2024-01-24 11:02:16 -0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+    val myByte: Byte = 42
+    val myShort: Short = 1000
+    val myInt: Int = 123456
+    val myLong: Long = 9876543210L
+    val myFloat: Float = 3.14F
+    val myDouble: Double = 2.71828
+    val myBoolean: Boolean = true
+    val myChar: Char = 'A'
+
+    println("byte demo: $myByte")
+    println("short demo: $myShort")
+    println("int demo: $myInt")
+    println("long demo: $myLong")
+    println("float demo: $myFloat")
+    println("double demo: $myDouble")
+    println("boolean demo: $myBoolean")
+    println("char demo: $myChar")
+}
